07430335a4b2fb8131f663224fcad45fe3de7705
unknown <milesjoh001@warren.k12.in.us>
2018-12-18 13:07:56 -0500

    finished???

--- resources/final.js
+++ resources/final.js
@@ -19,16 +19,4 @@ $(document).ready(function(){
         $('.dailylifeimg').slideDown(2000);
         $('.dailylifewords').slideUp(2000);
     })    
-    var $videoSrc;  
-    $('.video-btn').click(function() {
-        $videoSrc = $(this).data( "src" );
-    });
-    console.log($videoSrc);
-
-    $('#myModal').on('shown.bs.modal', function (e) { 
-    $("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" ); 
-    })
-    $('#myModal').on('hide.bs.modal', function (e) {
-        $("#video").attr('src',$videoSrc); 
-    }) 
 });
